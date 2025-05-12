fb290748b85cb2d11bf99f65df061d452795d500
ugurkayacan <59063217+ugurkayacan@users.noreply.github.com>
2025-05-12 21:06:37 +0300

    Düzeltme

--- yonetici[2021]/Aidat_Bilgisi_Ve_Gelirler.Designer.cs
+++ yonetici[2021]/Aidat_Bilgisi_Ve_Gelirler.Designer.cs
@@ -67,21 +67,23 @@ namespace yonetici_2021_
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.OrangeRed;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(46, 44);
+            this.label1.Location = new System.Drawing.Point(61, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 18);
+            this.label1.Size = new System.Drawing.Size(192, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Yıllara göre Aydatlar";
+            this.label1.Text = "Yıllara göre Aidatlar";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.OrangeRed;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(90, 343);
+            this.button1.Location = new System.Drawing.Point(120, 422);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 61);
+            this.button1.Size = new System.Drawing.Size(109, 75);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Yeni bir aydat ekle";
+            this.button1.Text = "Yeni bir aidat ekle";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -90,9 +92,10 @@ namespace yonetici_2021_
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.OrangeRed;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(532, 44);
+            this.label2.Location = new System.Drawing.Point(709, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.Size = new System.Drawing.Size(78, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Gelirler";
             // 
@@ -104,9 +107,10 @@ namespace yonetici_2021_
             this.yururluk,
             this.columnHeader4});
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(46, 74);
+            this.listView2.Location = new System.Drawing.Point(61, 91);
+            this.listView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(344, 250);
+            this.listView2.Size = new System.Drawing.Size(457, 307);
             this.listView2.TabIndex = 9;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -136,11 +140,12 @@ namespace yonetici_2021_
             // 
             this.button2.BackColor = System.Drawing.Color.OrangeRed;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(179, 343);
+            this.button2.Location = new System.Drawing.Point(239, 422);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 61);
+            this.button2.Size = new System.Drawing.Size(109, 75);
             this.button2.TabIndex = 10;
-            this.button2.Text = "aydat sil";
+            this.button2.Text = "aidat sil";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -148,11 +153,12 @@ namespace yonetici_2021_
             // 
             this.button3.BackColor = System.Drawing.Color.OrangeRed;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button3.Location = new System.Drawing.Point(266, 343);
+            this.button3.Location = new System.Drawing.Point(355, 422);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(82, 60);
+            this.button3.Size = new System.Drawing.Size(109, 74);
             this.button3.TabIndex = 11;
-            this.button3.Text = "Aydat güncelle";
+            this.button3.Text = "Aidat güncelle";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -160,9 +166,10 @@ namespace yonetici_2021_
             // 
             this.button4.BackColor = System.Drawing.Color.OrangeRed;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button4.Location = new System.Drawing.Point(535, 344);
+            this.button4.Location = new System.Drawing.Point(713, 423);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(76, 52);
+            this.button4.Size = new System.Drawing.Size(101, 64);
             this.button4.TabIndex = 12;
             this.button4.Text = "Gelir ekle";
             this.button4.UseVisualStyleBackColor = false;
@@ -172,9 +179,10 @@ namespace yonetici_2021_
             // 
             this.button5.BackColor = System.Drawing.Color.OrangeRed;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button5.Location = new System.Drawing.Point(628, 343);
+            this.button5.Location = new System.Drawing.Point(837, 422);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(76, 52);
+            this.button5.Size = new System.Drawing.Size(101, 64);
             this.button5.TabIndex = 13;
             this.button5.Text = "Gelir sil";
             this.button5.UseVisualStyleBackColor = false;
@@ -184,9 +192,10 @@ namespace yonetici_2021_
             // 
             this.button6.BackColor = System.Drawing.Color.OrangeRed;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button6.Location = new System.Drawing.Point(726, 344);
+            this.button6.Location = new System.Drawing.Point(968, 423);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(76, 52);
+            this.button6.Size = new System.Drawing.Size(101, 64);
             this.button6.TabIndex = 14;
             this.button6.Text = "Gelir güncelle";
             this.button6.UseVisualStyleBackColor = false;
@@ -197,9 +206,10 @@ namespace yonetici_2021_
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(118, 422);
+            this.label3.Location = new System.Drawing.Point(157, 519);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 18);
+            this.label3.Size = new System.Drawing.Size(59, 24);
             this.label3.TabIndex = 15;
             this.label3.Text = "miktar";
             // 
@@ -208,9 +218,10 @@ namespace yonetici_2021_
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(66, 458);
+            this.label5.Location = new System.Drawing.Point(88, 564);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 18);
+            this.label5.Size = new System.Drawing.Size(129, 24);
             this.label5.TabIndex = 16;
             this.label5.Text = "Yürürlük tarihi:";
             // 
@@ -219,24 +230,27 @@ namespace yonetici_2021_
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(49, 489);
+            this.label6.Location = new System.Drawing.Point(65, 602);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 18);
+            this.label6.Size = new System.Drawing.Size(150, 24);
             this.label6.TabIndex = 17;
             this.label6.Text = "son ödeme tarihi";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 424);
+            this.textBox1.Location = new System.Drawing.Point(239, 522);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 18;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(179, 490);
+            this.textBox4.Location = new System.Drawing.Point(239, 603);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
             this.textBox4.TabIndex = 20;
             // 
             // label7
@@ -244,33 +258,37 @@ namespace yonetici_2021_
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.White;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(408, 88);
+            this.label7.Location = new System.Drawing.Point(544, 108);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(92, 18);
+            this.label7.Size = new System.Drawing.Size(118, 24);
             this.label7.TabIndex = 21;
             this.label7.Text = "silinecek veri";
             // 
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(411, 127);
+            this.textBox5.Location = new System.Drawing.Point(548, 156);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
             this.textBox5.TabIndex = 22;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(280, 454);
+            this.dateTimePicker1.Location = new System.Drawing.Point(373, 559);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 23;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(179, 455);
+            this.textBox3.Location = new System.Drawing.Point(239, 560);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
             this.textBox3.TabIndex = 19;
             // 
             // label9
@@ -278,9 +296,10 @@ namespace yonetici_2021_
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.White;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label9.Location = new System.Drawing.Point(510, 423);
+            this.label9.Location = new System.Drawing.Point(680, 521);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(84, 18);
+            this.label9.Size = new System.Drawing.Size(103, 24);
             this.label9.TabIndex = 25;
             this.label9.Text = "Gelir miktar";
             // 
@@ -289,24 +308,27 @@ namespace yonetici_2021_
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.White;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label10.Location = new System.Drawing.Point(518, 458);
+            this.label10.Location = new System.Drawing.Point(691, 564);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(67, 18);
+            this.label10.Size = new System.Drawing.Size(83, 24);
             this.label10.TabIndex = 26;
             this.label10.Text = "açıklama";
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(604, 423);
+            this.textBox7.Location = new System.Drawing.Point(805, 521);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.Size = new System.Drawing.Size(132, 22);
             this.textBox7.TabIndex = 28;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(604, 455);
+            this.textBox8.Location = new System.Drawing.Point(805, 560);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.Size = new System.Drawing.Size(132, 22);
             this.textBox8.TabIndex = 29;
             // 
             // listView3
@@ -316,9 +338,10 @@ namespace yonetici_2021_
             this.columnHeader2,
             this.columnHeader3});
             this.listView3.HideSelection = false;
-            this.listView3.Location = new System.Drawing.Point(535, 74);
+            this.listView3.Location = new System.Drawing.Point(713, 91);
+            this.listView3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView3.Name = "listView3";
-            this.listView3.Size = new System.Drawing.Size(260, 250);
+            this.listView3.Size = new System.Drawing.Size(345, 307);
             this.listView3.TabIndex = 30;
             this.listView3.UseCompatibleStateImageBehavior = false;
             this.listView3.View = System.Windows.Forms.View.Details;
@@ -337,14 +360,14 @@ namespace yonetici_2021_
             this.columnHeader3.Text = "açıklama";
             this.columnHeader3.Width = 134;
             // 
-            // Aydat_Bilgisi_Ve_Gelirler
+            // Aidat_Bilgisi_Ve_Gelirler
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(839, 585);
+            this.ClientSize = new System.Drawing.Size(1119, 720);
             this.Controls.Add(this.listView3);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -368,7 +391,8 @@ namespace yonetici_2021_
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "Aydat_Bilgisi_Ve_Gelirler";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Aidat_Bilgisi_Ve_Gelirler";
             this.Text = "Aydat Bilgisi Ve Gelirler";
             this.Load += new System.EventHandler(this.Aydat_Bilgisi_Ve_Gelirler_Load);
             this.ResumeLayout(false);
